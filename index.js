67a4901ad54a1c3b62e946303980244b2731b042
RupamSingh333 <rupamkumar333@gmail.com>
2023-02-25 12:35:51 +0530

    default route setup

--- index.js
+++ index.js
@@ -8,12 +8,16 @@ mongoose.connect("mongodb://127.0.0.1:27017/ECOM");
 const user_route = require("./routes/userRoute");
 app.use("/api", user_route);
 
-
-
 //Home Route
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Hello Welcome to API Home Page</h1>");
+});
 
+app.get('*', function(req, res){
+  res.send('what???', 404);
+});
 
 
 app.listen(5000, function () {
-  console.log("Server is Ready");
+  console.log("Server is Ready on the localhost:5000/");
 });
