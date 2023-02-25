e3fb7c3b2199eefcac49d7f05308775bb5a6bd00
RupamSingh333 <rupamkumar333@gmail.com>
2023-02-25 17:06:36 +0530

    add 404 error and home page add in thml

--- index.js
+++ index.js
@@ -10,11 +10,85 @@ app.use("/api", user_route);
 
 //Home Route
 app.get("/", (req, res) => {
-  res.status(200).send("<h1>Hello Welcome to API Home Page</h1>");
+  res.status(200).send(`<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Welcome to My Node.js Server</title>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          background-color: #f2f2f2;
+        }
+        .container {
+          margin: 100px auto;
+          text-align: center;
+        }
+        h1 {
+          font-size: 5em;
+          color: #555;
+          margin-bottom: 0.2em;
+        }
+        p {
+          font-size: 1.2em;
+          color: #777;
+          margin-top: 0.2em;
+        }
+        a {
+          color: #999;
+          text-decoration: none;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="container">
+        <h1>Welcome to My Node.js Server</h1>
+        <p>This is a simple Node.js application.</p>
+        <a href="/users">View All Users</a>
+      </div>
+    </body>
+  </html>
+  `);
 });
 
 app.get('*', function(req, res){
-  res.status(404).send('<h1>Opps Not Found</h1>');
+  res.status(404).send(`<!DOCTYPE html>
+  <html>
+    <head>
+      <title>404 Not Found</title>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          background-color: #f2f2f2;
+        }
+        .container {
+          margin: 100px auto;
+          text-align: center;
+        }
+        h1 {
+          font-size: 5em;
+          color: #555;
+          margin-bottom: 0.2em;
+        }
+        p {
+          font-size: 1.2em;
+          color: #777;
+          margin-top: 0.2em;
+        }
+        a {
+          color: #999;
+          text-decoration: none;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="container">
+        <h1>404</h1>
+        <p>Oops! The page you requested was not found.</p>
+        <a href="/">Go back to the home page</a>
+      </div>
+    </body>
+  </html>
+  `);
 });
 
 
